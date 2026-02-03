cfebe98c1677790dd01b15f88c2e9c154956673b
PujanPraj <puzpraz123@gmail.com>
2026-02-03 16:10:08 +0545

    project setup

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+export const app = express();
+
+import dotnet from "dotenv";
+dotnet.config();
+
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
